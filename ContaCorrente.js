a524c265d347120b8c596f977f0adf89269f40f5
Sérgio Lima <sergiolimafg3@gmail.com>
2023-02-28 08:42:21 -0300

    criado a classe conta, reutilizando -a com o super para reeuso de codigo.

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,47 +1,12 @@
 import { Cliente } from "./Cliente.js";
+import { Conta } from "./Conta.js";
 
 // #saldo = 0 https://github.com/tc39/proposal-class-fields#private-fields 
-export class ContaCorrente {
+export class ContaCorrente extends Conta {
     static numeroDeContas = 0;
-    
-      set cliente(novoValor) {
-        if(novoValor instanceof Cliente){
-            this._cliente = novoValor;
-        }
-       
-    }
-        get cliente() {
-            return this._cliente;
-}
-    get saldo() {
-        return this._saldo;
-}
 
-constructor(agencia, cliente){
-    this.agencia = agencia;
-    this.cliente = cliente;
-    this._saldo = 0;
+constructor(cliente, agencia) {
+    super(0,cliente,agencia);
     ContaCorrente.numeroDeContas += 1;
 }
-
-    // Regra de Negocio sacar === tem que ter saldo!
-    sacar(valor) { // dentro dos parenteses estão os parametros ou argumentos
-        if(this._saldo >= valor) {
-            this._saldo -= valor;
-                return valor;
-        }else {
-            console.log('Saldo Insuficiente!');
-        }
-    }
-    depositar(valor) {
-        if(valor <= 0) {
-            return;     
-        } 
-        this._saldo += valor;        
-    }
-
-    transferir(valor, conta) {
-        const valoSacado = this.sacar(valor);
-        conta.depositar(valoSacado);
-    }
 }
