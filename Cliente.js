6b3e1bf33a1f5f2c33ca6f392e066f240997838e
Sérgio Lima <sergiolimafg3@gmail.com>
2023-03-06 17:23:47 -0300

    criado cliente e autenticado

--- Cliente.js
+++ Cliente.js
@@ -3,9 +3,13 @@ export class Cliente {
     return this._cpf;
     }
 
-    constructor(nome, cpf) {
+    constructor(nome, cpf, senha) {
         this._nome = nome;
         this._cpf = cpf;
+        this._senha = senha;
+    }
+    autenticar() {
+        return true;
     }
     
 }
--- index.js
+++ index.js
@@ -9,6 +9,10 @@ diretor.cadastrarSenha("123456");
 const gerente = new Gerente("Rodrigo", 10000, 12345678900);
 gerente.cadastrarSenha("123");
 
-const estaLogado = SistemaAutenticacao.login(gerente, "123");
+const cliente = new Cliente("Lais", 78945612300, "456")
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
 
-console.log(estaLogado);
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "456");
+
+console.log(gerenteEstaLogado, diretorEstaLogado, clienteEstaLogado);
