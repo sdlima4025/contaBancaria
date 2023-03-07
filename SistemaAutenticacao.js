872a37fc34352456ea2e504d04df621537774a8e
Sérgio Lima <sergiolimafg3@gmail.com>
2023-03-07 10:29:55 -0300

    finalizado projeto, apenas restando organizar nas devidas pastas criadas

--- SistemaAutenticacao.js
+++ SistemaAutenticacao.js
@@ -1,4 +1,13 @@
-// ser autenticavel siginifica ter o metodo autenticar
+/* ser autenticavel siginifica ter o metodo autenticar.
+Base OO == Polimorfismo ?
+Duck Typing == não importa se é um pato, mais se o comportamento 
+é de um Pato!
+Encapssulamento para proteger os dados, exemplo conta corrente,
+usamos os acessores Get e Set para tal.
+herança ... ?
+classes e metodos abstratos & ou privados.
+Abstração!!!
+*/
 
 export class SistemaAutenticacao {
     static login(autenticavel, senha) {
@@ -12,3 +21,15 @@ export class SistemaAutenticacao {
         autenticavel.autenticar instanceof Function;
     }
 } 
+
+/* O que aprendemos nessa aula:
+
+operador in
+
+operador instanceOf
+
+Como proteger nossos sistemas
+
+interfaces padronizadas
+
+ Ducky Type */
